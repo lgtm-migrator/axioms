f7b9955112d68616bf10a9db1b286a7742115a7e
MickVanDuijn <m.vanduijn@zefiros.eu>
2019-04-05 23:00:38 +0200

    feat(omit-by): add omitBy

---
+++ src/omit-by.ts
@@ -0,0 +1,9 @@
+import { negate } from './negate'
+import { pickBy } from './pick-by'
+
+export function omitBy<O extends object>(
+    obj: O | undefined,
+    omitIf: (o: O[typeof k], k: keyof O) => boolean
+): Partial<O> {
+    return pickBy(obj, negate(omitIf))
+}
---
+++ test/omit-by.spec.ts
@@ -0,0 +1,29 @@
+import { omitBy } from '~/omit-by'
+
+describe('omitBy', () => {
+    function staticTrue() {
+        return true
+    }
+    test('undefined gives empty object', () => {
+        expect(omitBy(undefined, staticTrue)).toEqual({})
+    })
+
+    test('omits all properties if the function is statically true', () => {
+        expect(omitBy({ foo: 'foo', bar: 'bar' }, staticTrue)).toEqual({})
+    })
+
+    test('can filter based on key', () => {
+        expect(omitBy({ foo: 'foo', bar: 'bar' }, (_, k) => k === 'foo')).toEqual({ bar: 'bar' })
+    })
+
+    test('can filter based on value', () => {
+        expect(omitBy({ foo: 'bar', bar: 'foo' }, v => v === 'foo')).toEqual({ foo: 'bar' })
+    })
+
+    test('creates shallow clone of the object', () => {
+        const orig = { foo: 'bar', bar: 'foo' }
+        const filtered = omitBy(orig, v => v === 'foo')
+        orig.foo = 'foo'
+        expect(filtered).toEqual({ foo: 'bar' })
+    })
+})
