d7f7b7b6bd47356ec1a1ba7421707fdf649ed0fd
pevisscher <2388946+pevisscher@users.noreply.github.com>
2019-09-09 21:05:54 +0200

    feat(chunk): implement chunk function (#3)

---
+++ src/chunk.ts
@@ -0,0 +1,9 @@
+export function chunk<T>(arr: Readonly<T[]>, size: number = 1): T[][] {
+    size = Math.max(size, 1)
+    const cache: T[][] = []
+    const tmp = [...arr]
+    while (tmp.length) {
+        cache.push(tmp.splice(0, size))
+    }
+    return cache
+}
---
+++ test/chunk.spec.ts
@@ -0,0 +1,25 @@
+import { chunk } from '~/chunk'
+
+test('simple chunk function', () => {
+    expect(chunk([1, 2, 3, 4, 5])).toEqual([[1], [2], [3], [4], [5]])
+})
+
+test('simple chunk function, other offset 3', () => {
+    expect(chunk([1, 2, 3, 4, 5], 3)).toEqual([[1, 2, 3], [4, 5]])
+})
+
+test('simple chunk function, other offset 5', () => {
+    expect(chunk([1, 2, 3, 4, 5], 5)).toEqual([[1, 2, 3, 4, 5]])
+})
+
+test('simple chunk function larger than array', () => {
+    expect(chunk([1, 2, 3, 4, 5], 10)).toEqual([[1, 2, 3, 4, 5]])
+})
+
+test('simple chunk function offset 0', () => {
+    expect(chunk([1, 2, 3, 4, 5], 0)).toEqual([[1], [2], [3], [4], [5]])
+})
+
+test('simple chunk function offset negative', () => {
+    expect(chunk([1, 2, 3, 4, 5], -5)).toEqual([[1], [2], [3], [4], [5]])
+})
