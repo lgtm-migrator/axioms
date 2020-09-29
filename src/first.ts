24cf55c58ef66a07a058f776b5ecdaf9ae6bf243
MickVanDuijn <m.vanduijn@zefiros.eu>
2020-09-29 22:40:48 +0200

    chore: update  to latest standards

--- src/first.ts
+++ src/first.ts
@@ -1,3 +1,3 @@
-export function first<T>(array: ReadonlyArray<T> | undefined | null, count: number = 1): T[] {
-    return (array || []).slice(0, Math.max(0, count))
+export function first<T>(array: ReadonlyArray<T> | undefined | null, count = 1): T[] {
+    return (array ?? []).slice(0, Math.max(0, count))
 }
