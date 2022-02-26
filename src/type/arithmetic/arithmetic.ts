291cf6d6841d18cdf07ef235ca5d8e80b58c4a97
MickVanDuijn <m.vanduijn@zefiros.eu>
2022-02-26 16:06:48 +0100

    feat(ci): setup full pipeline (#63)

--- src/type/arithmetic/arithmetic.ts
+++ src/type/arithmetic/arithmetic.ts
@@ -1,6 +1,7 @@
-type _BuildTuple<L extends number, T extends any[] = []> =
-    T extends { length: L } ? T : _BuildTuple<L, [...T, any]>
+type _BuildTuple<L extends number, T extends any[] = []> = T extends { length: L } ? T : _BuildTuple<L, [...T, any]>
 
-export type Subtract<A extends number, B extends number> = _BuildTuple<A> extends [...(infer R), ..._BuildTuple<B>] ? R['length'] : 0
+export type Subtract<A extends number, B extends number> = _BuildTuple<A> extends [...infer R, ..._BuildTuple<B>]
+    ? R['length']
+    : 0
 
 export type Add<A extends number, B extends number> = [..._BuildTuple<A>, ..._BuildTuple<B>]['length']
