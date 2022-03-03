455890731af1f27f6978fa5fe1934c0cc4309b11
MickVanDuijn <m.vanduijn@zefiros.eu>
2022-03-03 21:29:15 +0100

    fix: make hasPropertiesDefined distributive over unions (#84)
    * fix: make hasPropertiesDefined distributive over unions
    
    * test(require-keys): add type tests

--- src/type/keys/keys.ts
+++ src/type/keys/keys.ts
@@ -1,7 +1,14 @@
 import type { EmptyObj } from '../object'
+import type { SimplifyOnce } from '../simplify'
 
 export type KeyOf<T> = T extends unknown ? keyof T : never
 export type KeysOfType<T, U> = { [K in keyof T]: T[K] extends U ? K : never }[keyof T]
 export type RequiredKeys<T> = Exclude<KeysOfType<T, Exclude<T[keyof T], undefined>>, undefined>
 export type OptionalKeys<T> = Exclude<keyof T, RequiredKeys<T>>
-export type RequireKeys<T extends EmptyObj, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
+export type RequireKeys<T extends EmptyObj, K extends KeyOf<T>> = T extends infer S
+    ? SimplifyOnce<Omit<S, K> & Required<Pick<S, K & keyof S>>> extends infer U
+        ? U extends Record<K, unknown>
+            ? U
+            : never
+        : never
+    : never
