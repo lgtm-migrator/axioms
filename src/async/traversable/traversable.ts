a5f7231474a14d1bfd1d681d3611f06133224744
Mick van Duijn <m.vanduijn@zefiros.io>
2022-06-14 13:33:32 +0200

    feat(async): implement toAsyncTraversable

--- src/async/traversable/traversable.ts
+++ src/async/traversable/traversable.ts
@@ -7,3 +7,35 @@ export type AsyncMappable<T, R = unknown> =
     | AsyncTraverser<T, R>
     | Mappable<T, R>
     | (() => AsyncGenerator<T, R, void>)
+
+export type ToAsyncTraverser<Xs extends AsyncMappable<unknown, unknown>> = Xs extends AsyncMappable<infer T, infer R>
+    ? AsyncTraverser<T, R>
+    : never
+export function toAsyncTraverser<Xs extends AsyncMappable<unknown, unknown>>(xs: Xs): ToAsyncTraverser<Xs> {
+    //return isIterable(xs) ? xs[Symbol.iterator]() : isGeneratorFunction(xs) ? xs() : xs
+    // inlined
+    if (typeof xs === 'string' || Symbol.iterator in xs) {
+        return (xs as Traversable<unknown, unknown>)[Symbol.iterator]() as ToAsyncTraverser<Xs>
+    } else if (Symbol.asyncIterator in xs) {
+        return (xs as { [Symbol.asyncIterator](): AsyncIterator<unknown, unknown, void> })[
+            Symbol.asyncIterator
+        ]() as ToAsyncTraverser<Xs>
+    } else if (typeof xs === 'function' && xs.constructor !== null && xs.constructor.name === 'GeneratorFunction') {
+        return xs() as unknown as ToAsyncTraverser<Xs>
+    }
+    return xs as unknown as ToAsyncTraverser<Xs>
+}
+
+export type ToAsyncTraversable<Xs extends AsyncMappable<unknown, unknown>> = Xs extends AsyncMappable<infer T, infer R>
+    ? Traversable<T, R>
+    : never
+export function toAsyncTraversable<Xs extends AsyncMappable<unknown, unknown>>(xs: Xs): ToAsyncTraversable<Xs> {
+    //return isIterable(xs) ? xs : isGeneratorFunction(xs) ? xs() : { [Symbol.iterator]: () => xs }
+    // inlined
+    if (typeof xs === 'string' || Symbol.iterator in xs || Symbol.asyncIterator in xs) {
+        return xs as unknown as ToAsyncTraversable<Xs>
+    } else if (typeof xs === 'function' && xs.constructor !== null && xs.constructor.name === 'GeneratorFunction') {
+        return xs() as unknown as ToAsyncTraversable<Xs>
+    }
+    return { [Symbol.asyncIterator]: () => xs } as unknown as ToAsyncTraversable<Xs>
+}
