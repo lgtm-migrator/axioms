13d1dbef2bdd853a1df901ce7e4741237c884b26
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-08-12 14:38:26 +0200

    refactor: simplify isIterable interface (#106)

--- src/guard/is-iterable/is-iterable.spec.ts
+++ src/guard/is-iterable/is-iterable.spec.ts
@@ -10,3 +10,10 @@ test('array is iterable', () => {
 test('unknown is not iterable', () => {
     forAll(unknown({ string: false, array: false }), (xs) => !isIterable(xs))
 })
+
+test('inferrence', () => {
+    const xs: number[] | number = [1, 2, 3] as unknown as number[] | number
+    if (isIterable(xs)) {
+        expect(xs[0] * 2).toBe(2)
+    }
+})
